b7b8686aa9e239df6616994c1af30d22ee251b80
slashback100 <christophe.desirant@gmail.com>
2020-07-01 16:15:27 +0200

    commit

--- arduinoProjects/domoMqtt/src/inOutArduino_mqtt.ino
+++ arduinoProjects/domoMqtt/src/inOutArduino_mqtt.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>                  // For networking
 #include <Ethernet.h>             // For networking
 #include <PubSubClient.h>         // For MQTT
-#include <TimerOne.h>         // For MQTT
+#include <TimerOne.h>             // For Watchdog
 
 //#define DEBUG
-//#define DEBUGMQTT
+//#define DEBUGSERIAL
 #define ETAGE1A
 
 #ifdef ETAGE0A
@@ -49,15 +49,15 @@ char messBuffer[100];
 /* ------------- button management -------------------------------- */
 long lastActivityTime = 0;
 
-const int nbOutput = 14;
-const int nbInput = 33;
+const int nbOutput = 16;
+const int nbInput = 39;
 const int nbPir = 1;
-static int lastButtonState[nbInput] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-static int lastButtonLevel[nbInput] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,HIGH,HIGH, HIGH};
-static int buttonArray[nbInput] = {14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47};
+static int lastButtonState[nbInput] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0};
+static int lastButtonLevel[nbInput] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,HIGH,HIGH, HIGH, HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+static int buttonArray[nbInput] = {14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,A0,A1,A2,A3,A4,A5};
 static int lastPirState[nbPir] = {0};
 static int pirArray[nbPir] = {44};
-static int outputArray[nbOutput] = {1,2,3,4,5,6,7,8,9,11,12,13,48,49};
+static int outputArray[nbOutput] = {1,2,3,4,5,6,7,8,9,11,12,13,48,49,A14,A15};
 int lastStatus = 0;
 #define DEBOUNCE_DELAY 50
 //#define WATCHDOG 21600000
@@ -90,7 +90,7 @@ void sendCallback(String t){
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    #ifdef DEBUG
+    #ifdef DEBUGSERIAL
     Serial.print("Attempting MQTT connection...");
     #endif
     // Attempt to connect
@@ -98,7 +98,7 @@ void reconnect() {
     clientString.toCharArray(buffer, clientString.length()+1);
     
     if (client.connect(buffer, "slashback", "nimda")) {
-      #ifdef DEBUG
+      #ifdef DEBUGSERIAL
       Serial.println("connected");
       #endif
       // Once connected, publish an announcement...
@@ -118,7 +118,7 @@ void reconnect() {
       sendMessage("log/info/"+arduinoId, "connected");
       sendMessage("init/"+arduinoId+"/ready", "ready");
     } else {
-      #ifdef DEBUG
+      #ifdef DEBUGSERIAL
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -147,6 +147,9 @@ void processButtonDigital(int buttonId){
   int sensorReading = digitalRead(buttonArray[buttonId]);
   if (sensorReading != lastButtonLevel[buttonId]){
     #ifdef DEBUG
+    sendMessage("log/info/"+arduinoId, "sensor!=lastbutton " + String(buttonId));
+    #endif
+    #ifdef DEBUGSERIAL
     Serial.println("sensor!=lastButton");
     #endif
     lastActivityTime = millis();
@@ -154,11 +157,17 @@ void processButtonDigital(int buttonId){
   if((sensorReading == LOW && lastButtonState[buttonId] == 0) /*was high*/ ||
     (sensorReading == HIGH && lastButtonState[buttonId] == 1)) {  // potential change of state
     #ifdef DEBUG
+    sendMessage("log/info/"+arduinoId, "pot change of state " + String(buttonId));
+    #endif
+    #ifdef DEBUGSERIAL
     //Serial.println("change of state");
     #endif
     if((millis() - lastActivityTime) > DEBOUNCE_DELAY){  // Proceed if we haven't seen a recent event on this button
-      #ifdef DEBUG
+      #ifdef DEBUGSERIAL
       //Serial.println("> debounce delay");
+      #endif
+      #ifdef DEBUG
+      sendMessage("log/info/"+arduinoId, "above debounce_delay " + String(buttonId));
       #endif
       lastActivityTime = millis();
 
@@ -195,17 +204,17 @@ void longWDT(void)
  */
 void setup(){
   /* -------------- network ------------------- */
-  #ifdef DEBUG
+  #ifdef DEBUGSERIAL
   Serial.begin(9600); 
   Serial.print(F("Using static MAC address: "));
   #endif
   char tmpBuf[17];
   sprintf(tmpBuf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  #ifdef DEBUG
+  #ifdef DEBUGSERIAL
   Serial.println(tmpBuf);
   #endif
   Ethernet.begin(mac, ip);  // Use static address defined above
-  #ifdef DEBUG
+  #ifdef DEBUGSERIAL
   Serial.print(F("My IP: http://"));
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
@@ -219,12 +228,12 @@ void setup(){
   #endif
 
   /* --------- prepare MQTT connection ------------- */
-  #ifdef DEBUG
+  #ifdef DEBUGSERIAL
   Serial.println("connecting...");
   #endif
   client.setServer(broker, 1883);
   client.setCallback(callback);
-  #ifdef DEBUG
+  #ifdef DEBUGSERIAL
   Serial.println("Ready.");
   #endif
   
@@ -269,7 +278,7 @@ void loop(){
   if(messageReceived){
       if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/out/").length()) == "cmd/"+arduinoId+"/out/"){
           int pin = receivedTopic.substring(("cmd/"+arduinoId+"/out/").length()).toInt(); //remove prefix
-          if(pin >= 0 /* 0 & 1 are for tx rx */  && pin <= 49 /* 50 -> 53 are for SPI */){
+          if(pin >= 0 /* 0 & 1 are for tx rx, can be used iif serial is disabled */  && pin <= 70 /* 50 -> 53 are for SPI */){
             if(receivedPayload == "ON"){
               digitalWrite(pin, LOW);
               #ifdef DEBUG
@@ -290,11 +299,15 @@ void loop(){
             sendCallback(receivedTopic);
           }
       } else if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/watchdog").length()) == "cmd/"+arduinoId+"/watchdog"){
+        #ifdef DEBUG
         sendMessage("log/debug/"+arduinoId, "Reset of the watchdog");
+        #endif
 	lastUpdate=millis(); 
 	//Timer1.restart();
       } else if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/reset").length()) == "cmd/"+arduinoId+"/reset"){
+        #ifdef DEBUG
         sendMessage("log/debug/"+arduinoId, "Reset of the arduino");
+        #endif
 	resetFunc();
       } else {
         sendMessage("log/error/"+arduinoId, receivedTopic);
