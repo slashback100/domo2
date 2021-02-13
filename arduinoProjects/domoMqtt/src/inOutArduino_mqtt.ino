5328a5d402b3901c7301aaa2a1a1eab320348abc
slashback100 <43109757+slashback100@users.noreply.github.com>
2021-02-13 12:15:27 +0100

    Avoid usage of String

--- arduinoProjects/domoMqtt/src/inOutArduino_mqtt.ino
+++ arduinoProjects/domoMqtt/src/inOutArduino_mqtt.ino
@@ -1,42 +1,41 @@
 #include <SPI.h>                  // For networking
 #include <Ethernet.h>             // For networking
 #include <PubSubClient.h>         // For MQTT
-#include <TimerOne.h>             // For Watchdog
-#include <Wire.h>	          // For I2C (variator)
+//#include <TimerOne.h>             // For Watchdog
+#include <Wire.h>              // For I2C (variator)
 
 
 //#define DEBUGSERIAL
-#define ETAGE1A
-
+#define ETAGE3A
+#define idLength 7
 #ifdef ETAGE0A
 static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEA };
 IPAddress ip(192,168,0,203);
-String arduinoId = "etage0a";
+const char* arduinoId = "etage0a";
 #endif
 #ifdef ETAGE1A
 static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,0,200);
-String arduinoId = "etage1a";
+const char* arduinoId = "etage1a";
 #endif
 #ifdef ETAGE2A
 static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
 IPAddress ip(192,168,0,201);
-String arduinoId = "etage2a";
+const char* arduinoId = "etage2a";
 #endif
 #ifdef ETAGE2B
 static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };
 IPAddress ip(192,168,0,202);
-String arduinoId = "etage2b";
+const char* arduinoId = "etage2b";
+#endif
+#ifdef ETAGE3A
+static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xFA };
+IPAddress ip(192,168,0,204);
+const char* arduinoId = "etage3a";
 #endif
 
 /*--------------------------- Variator -----------------------------*/
-volatile int val; // variable used by master to send data to slave
-
-/*-------------------------- log mngmnt ----------------------------*/
-boolean debug = false;
-void log(String level, String msg){
-    sendMessage("log/"+level+"/"+arduinoId, msg);
-}
+volatile unsigned int val; // variable used by master to send data to slave
 
 /*--------------------------- Network ------------------------------*/
 /* Network Settings */
@@ -46,47 +45,63 @@ EthernetClient ethclient;
 IPAddress broker(192,168,0,186);
 PubSubClient client(ethclient);
 boolean messageReceived = false;
-String receivedTopic;
-String receivedPayload;
+char receivedTopic[50];
+char receivedPayload[50];
 char buffer[50];
-char topicBuffer[30];
-char messBuffer[100];
+char buffer2[100];
+char topic[50];
+/*-------------------------- log mngmnt ----------------------------*/
+boolean debug = false;
+void logError(char* msg){
+    sprintf(topic, "log/%s/error", arduinoId);
+    sendMessage(topic, msg);
+}
+void logInfo(char* msg){
+    sprintf(topic, "log/%s/info", arduinoId);
+    sendMessage(topic, msg);
+}
+void logDebug(char* msg){
+    sprintf(topic, "log/%s/debug", arduinoId);
+    sendMessage(topic, msg);
+}
+void log(char* level, char* msg){
+    sprintf(topic, "log/%s/%s", arduinoId, level);
+    sendMessage(topic, msg);
+}
+
 /* ------------- button management -------------------------------- */
 long lastActivityTime = 0;
 
-const int nbOutput = 16;
-const int nbInput = 40;
-const int nbPir = 0;
-static int lastButtonState[nbInput] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0};
-static int lastButtonLevel[nbInput] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,HIGH,HIGH, HIGH, HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
-static int buttonArray[nbInput] = {14,15,16,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,A0,A1,A2,A3,A4,A5,A7,A8,A9};//20 & 21 are for i2c (SDL & SDA)
-static int lastPirState[nbPir]; // = {0};
-static int pirArray[nbPir]; // = {44};
-static int outputArray[nbOutput] = {1,2,3,4,5,6,7,8,9,11,12,13,48,49,A14,A15};
-int lastStatus = 0;
+const unsigned int nbOutput = 16;
+const unsigned int nbInput = 40;
+const unsigned int nbPir = 0;
+static boolean lastButtonState[nbInput] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+static unsigned int lastButtonLevel[nbInput] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,HIGH,HIGH, HIGH, HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+static unsigned int buttonArray[nbInput] = {14,15,16,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,A0,A1,A2,A3,A4,A5,A7,A8,A9};//20 & 21 are for i2c (SDL & SDA)
+static unsigned int watchdogResetPin = A10;
+//static boolean lastPirState[nbPir]; // = {0};
+static unsigned int pirArray[nbPir]; // = {44};
+static unsigned int outputArray[nbOutput] = {1,2,3,4,5,6,7,8,9,11,12,13,48,49,A14,A15};
+boolean watchdogStatus;
 #define DEBOUNCE_DELAY 50 // button push delay
-#define WATCHDOG 360000 // 6min
 
 /* ------------- MQTT in -------------------------------- */
 void callback(char* topic, byte* payload, unsigned int l){
   if(!messageReceived){
     messageReceived=true;
-    receivedTopic = String(topic);
-    receivedPayload = "";
-    for (unsigned int i=0;i<l;i++) {
-      receivedPayload = receivedPayload + String((char)payload[i]);
-    }
+    strcpy(receivedTopic, topic);
+    strncpy(receivedPayload, (char*) payload, l);
+    receivedPayload[l] = '\0';
   }
 }
 
-void sendMessage(String topic, String message){
-  topic.toCharArray(topicBuffer, topic.length() + 1);
-  message.toCharArray(messBuffer, message.length() + 1);
-  client.publish(topicBuffer, messBuffer);
+void sendMessage(char* topic, char* message){
+  client.publish(topic, message);
 }
 
-void sendCallback(String t){
-    sendMessage("callback",  t);
+void sendCallback(char* t){
+    sprintf(topic, "callback");
+    sendMessage(topic,  t);
 }
 
 /* ------------- MQTT connection -------------------------------- */
@@ -94,42 +109,39 @@ void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     #ifdef DEBUGSERIAL
-    Serial.print("Attempting MQTT connection...");
+    Serial.print(F("Attempting MQTT connection..."));
     #endif
     // Attempt to connect
-    String clientString = "Reconnecting Arduino-" + String(Ethernet.localIP());
-    clientString.toCharArray(buffer, clientString.length()+1);
+    //strcpy(buffer, "Reconnecting Arduino-");
+    //strcat(buffer; Ethernet.localIP());
     
-    if (client.connect(buffer, "slashback", "nimda")) {
+    //String clientString = "Reconnecting Arduino-" + String(Ethernet.localIP());
+    //clientString.toCharArray(buffer, clientString.length()+1);
+    
+    if (client.connect(arduinoId, "slashback", "nimda")) {
       #ifdef DEBUGSERIAL
-      Serial.println("connected");
+      Serial.println(F("connected"));
       #endif
       // Once connected, publish an announcement...
       // ... and resubscribe;
-      String initString = "cmd/"+arduinoId+"/out/#";
-      initString.toCharArray(buffer, initString.length()+1);
-      client.subscribe(buffer);
 
-      initString = "cmd/"+arduinoId+"/reset";
-      initString.toCharArray(buffer, initString.length()+1);
-      client.subscribe(buffer);
+      sprintf(topic, "cmd/%s/out/#", arduinoId);
+      client.subscribe(topic);
 
-      initString = "cmd/"+arduinoId+"/watchdog";
-      initString.toCharArray(buffer, initString.length()+1);
-      client.subscribe(buffer);
+      sprintf(topic, "cmd/%s/resetWatchdog", arduinoId);
+      client.subscribe(topic);
 
-      initString = "cmd/"+arduinoId+"/debug";
-      initString.toCharArray(buffer, initString.length()+1);
-      client.subscribe(buffer);
+      sprintf(topic, "cmd/%s/debug", arduinoId);
+      client.subscribe(topic);
       
-      //sendMessage("log/info/"+arduinoId, "connected");
-      if(debug) log("info", "connected");
-      sendMessage("init/"+arduinoId+"/ready", "ready");
+      sprintf(topic, "init/%s/ready", arduinoId);
+      sprintf(buffer2, "ready");
+      sendMessage(topic, buffer2);
     } else {
       #ifdef DEBUGSERIAL
-      Serial.print("failed, rc=");
+      Serial.print(F("failed, rc="));
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      Serial.println(F(" try again in 5 seconds"));
       #endif
       // Wait 5 seconds before retrying
       delay(5000);
@@ -138,8 +150,9 @@ void reconnect() {
 }
 
 /* -------------------- Input ------------------------*/
+/*
 void processPir(int pirId){
-  int sensorReading = digitalRead(pirArray[pirId]) == HIGH ? 1 : 0;
+  unsigned int sensorReading = digitalRead(pirArray[pirId]) == HIGH ? 1 : 0;
   //if((sensorReading == 0 && lastPirState[pirId] == 1) || // was high
   //  (sensorReading == 1 && lastPirState[pirId] == 0)) {  // change of state
   if(sensorReading ^ lastPirState[pirId]){ // XOR
@@ -150,48 +163,41 @@ void processPir(int pirId){
       }
       lastPirState[pirId] = sensorReading;
   }
-}
+}*/
 void processButtonDigital(int buttonId){
-  int sensorReading = digitalRead(buttonArray[buttonId]);
+  unsigned int sensorReading = digitalRead(buttonArray[buttonId]);
   if (sensorReading != lastButtonLevel[buttonId]){
-    if(debug) log("info", "sensor!=lastbutton (" + String(buttonId)+")");
+    //if(debug) log("info", "sensor!=lastbutton (" + String(buttonId)+")");
+    if(debug) {
+      sprintf(buffer2, "sensor!=lastbutton (%d)", buttonId);
+      logDebug(buffer2);
+    }
     lastActivityTime = millis();
   }
-  if((sensorReading == LOW && lastButtonState[buttonId] == 0) /*was high*/ ||
-    (sensorReading == HIGH && lastButtonState[buttonId] == 1)) {  // potential change of state
-    if(debug) log("info", "Change of state? (" + String(buttonId)+")");
+  if((sensorReading == LOW && !lastButtonState[buttonId]) /*was high*/ ||
+    (sensorReading == HIGH && lastButtonState[buttonId])) {  // potential change of state
+    //if(debug) log(F("info"), "Change of state? (" + String(buttonId)+")");
+    if(debug) {
+      sprintf(buffer2, "Change of state? (%d)", buttonId);
+      logDebug(buffer2);
+    }
     if((millis() - lastActivityTime) > DEBOUNCE_DELAY){  // Proceed if we haven't seen a recent event on this button
-      if(debug) log("info", "Change of state! (" + String(buttonId)+")");
+      if(debug) {
+        sprintf(buffer2, "Change of state! (%d)", buttonId);
+        logDebug(buffer2);
+      }
+      //if(debug) log(F("info"), "Change of state! (" + String(buttonId)+")");
       lastActivityTime = millis();
 
-      if (sensorReading == LOW) {
-        sendMessage("cmd/"+arduinoId+"/in/"+buttonArray[buttonId], "ON");
-      } else {
-        sendMessage("cmd/"+arduinoId+"/in/"+buttonArray[buttonId], "OFF");
-      }
-      lastButtonState[buttonId] = sensorReading == LOW ? 1 : 0;
+      sprintf(topic, "cmd/%s/in/%d", arduinoId, buttonArray[buttonId]);
+      sprintf(buffer, sensorReading == LOW ? "ON" : "OFF");
+      sendMessage(topic, buffer);
+      lastButtonState[buttonId] = sensorReading == LOW;
     }
   }
   lastButtonLevel[buttonId] = sensorReading;
 }
-void(* resetFunc) (void) = 0;
 
-//last time the WDT was ACKd by the application
-unsigned long lastUpdate=0;
- 
-//time, in ms, after which a reset should be triggered
-unsigned long timeout=WATCHDOG;
- 
-void longWDT(void)
-{
-  if((millis()-lastUpdate) > timeout)
-  {
-    sei(); // activate interruptions
-    //detach Timer1 interrupt so that if processing goes long, WDT isn't re-triggered
-    Timer1.detachInterrupt();
-    resetFunc();
-  }
-}
 /**
  * Initial configuration
  */
@@ -233,21 +239,22 @@ void setup(){
   #endif
   
   /* --------- IO ------------- */
-  for(int i = 0; i < nbInput; i++){
+  for(unsigned int i = 0; i < nbInput; i++){
     pinMode(buttonArray[i], INPUT_PULLUP);
   }
-  for(int i = 0; i < nbPir; i++){
+  for(unsigned int i = 0; i < nbPir; i++){
     pinMode(pirArray[i], INPUT);
   }
-  for(int i = 0; i < nbOutput; i++){
+  for(unsigned int i = 0; i < nbOutput; i++){
     pinMode(outputArray[i], OUTPUT);
     digitalWrite(outputArray[i], HIGH);
   }
-  /* ----------- watchdog ------------ */
-  lastUpdate=millis();
-  Timer1.initialize(1000000); //1 second pulses
-  Timer1.attachInterrupt(longWDT); //code to execute
-  timeout = WATCHDOG;
+  /* --------- watchdog -------- */
+  pinMode(watchdogResetPin, OUTPUT);
+  digitalWrite(watchdogResetPin, LOW);
+  delayMicroseconds(100); // reset the watchdog on the esp when booting up
+  digitalWrite(watchdogResetPin, HIGH);
+  watchdogStatus=HIGH;
 }
 
 void loop(){
@@ -259,72 +266,115 @@ void loop(){
   client.loop();  
   
   /* ---------------- input pins -------------------*/
-  for(int i = 0; i < nbInput; i++) {
+  for(unsigned int i = 0; i < nbInput; i++) {
       processButtonDigital(i);
   }
 
-  /* ---------------- pir pins -------------------*/
-  for(int i = 0; i < nbPir; i++) {
-      processPir(i);
-  }
-  
   /* ---------- mqtt message received --------------*/
   if(messageReceived){
-      if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/out/").length()) == "cmd/"+arduinoId+"/out/"){
-          int pin = receivedTopic.substring(("cmd/"+arduinoId+"/out/").length()).toInt(); //remove prefix
+      // cmd/etagexx/out/
+      strcpy(buffer, receivedTopic);
+      buffer[idLength+9]='\0'; //substring
+
+      sprintf(buffer2, "cmd/%s/out/", arduinoId);
+
+      if(strcmp(buffer, buffer2) == 0){
+          // get the pin nbr, extrating from the topic
+          strcpy(buffer, receivedTopic +sizeof(char)*(idLength+9));
+          int pin = atoi(buffer);
           if(pin >= 0 /* 0 & 1 are for tx rx, can be used iif serial is disabled */  && pin <= 70 /* 50 -> 53 are for SPI */){
-            if(receivedPayload == "ON"){
+            if(debug){
+              sprintf(buffer2,"out on pin %d, msg %s", pin, receivedPayload);
+              logDebug(buffer2);
+            }
+            if(strcmp(receivedPayload, "ON") == 0){
               digitalWrite(pin, LOW);
-              if(debug) log("debug", "successfully treated message "+receivedTopic+" for arduino "+arduinoId+" pin "+String(pin));
-            } else if(receivedPayload == "OFF"){
+              if(debug) {
+                sprintf(buffer2, "successfully treated on message %s for arduino %s pin %d",receivedTopic, arduinoId, pin);
+		logDebug(buffer2);
+              }
+            } else if(strcmp(receivedPayload,"OFF") == 0){
               digitalWrite(pin, HIGH);
-              if(debug) log("debug", "successfully treated message "+receivedTopic+" for arduino "+arduinoId+" pin "+String(pin));
-            } else if(receivedPayload == "VON"){
+              if(debug){
+                 sprintf(buffer2, "successfully treated off message %s for arduino %s pin %d",receivedTopic, arduinoId, pin);
+                 logDebug(buffer2);
+              }
+            } else if(strcmp(receivedPayload,"VON") == 0){
               Wire.beginTransmission (9);
               Wire.write(pin);
               Wire.write(250); // 250 for on, 200 for off
               Wire.endTransmission ();
-              if(debug) log("debug", "successfully treated message "+receivedTopic+" for arduino "+arduinoId+" pin "+String(pin));
-            } else if(receivedPayload == "VOF"){
+              if(debug){
+                 sprintf(buffer2, "successfully treated von message %s for arduino %s pin %d",receivedTopic, arduinoId, pin);
+                 logDebug(buffer2);
+              }
+            } else if(strcmp(receivedPayload,"VOF") == 0){
               Wire.beginTransmission (9);
               Wire.write(pin);
               Wire.write(200); // 250 for on, 200 for off
               Wire.endTransmission ();
-              if(debug) log("debug", "successfully treated message "+receivedTopic+" for arduino "+arduinoId+" pin "+String(pin));
+              if(debug){
+                 sprintf(buffer2, "successfully treated vof message %s for arduino %s pin %d",receivedTopic, arduinoId, pin);
+                 logDebug(buffer2);
+              }
             } else {
               //means it is a percentage, to send in I2C
-              val = receivedPayload.toInt();
-              if(debug) log("debug", "Percentage received "+String(val));
+              val = atoi(receivedPayload);
+              if(debug){
+                 sprintf(buffer2, "Percentage received %d", val);
+                 logDebug(buffer2);
+              }
               Wire.beginTransmission (9);
               Wire.write(pin);
               Wire.write(val);
               Wire.endTransmission ();
-              if(debug) log("debug", "successfully treated message "+receivedTopic+" for arduino "+arduinoId+" pin "+String(pin));
+              if(debug){
+                 sprintf(buffer2, "successfully treated perentage message %s for arduino %s pin %d",receivedTopic, arduinoId, pin);
+                 logDebug(buffer2);
+              }
             }
             if(debug){
-                log("debug", "cmd: "+receivedTopic);
-                log("debug", "pin: "+String(pin));
+               sprintf(buffer2, "cmd: %s pin: %d", receivedTopic, pin);
+               logDebug(buffer2);
             }
             sendCallback(receivedTopic);
           }
-      } else if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/watchdog").length()) == "cmd/"+arduinoId+"/watchdog"){
-        if(debug) log("debug", "Reset of the watchdog");
-	lastUpdate=millis(); 
-	//Timer1.restart();
-      } else if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/reset").length()) == "cmd/"+arduinoId+"/reset"){
-        if(debug) log("debug", "Reset of the arduino");
-	resetFunc();
-      } else if(receivedTopic.substring(0, ("cmd/"+arduinoId+"/debug").length()) == "cmd/"+arduinoId+"/debug"){
-        if(receivedPayload == "true" || receivedPayload == "1" || receivedPayload == "ON" || receivedPayload == "TRUE"){
-          debug=true;
-	  log("info", "Log mode activated");
-        } else {
-          debug=false;
-	  log("info", "Log mode desactivated");
-        }
-      } else {
-        log("error", receivedTopic);
+      } else { 
+          //cmd/etagexx/resetWatchdog
+          sprintf(topic, "cmd/%s/resetWatchdog", arduinoId);
+          if(strcmp(receivedTopic, topic) == 0){
+            watchdogStatus=1-watchdogStatus;
+            digitalWrite(watchdogResetPin, watchdogStatus); //toggle the pin
+            if(debug){
+               sprintf(buffer2, "Watchdog reset ok");
+               logDebug(buffer2);
+               sprintf(buffer2, "free rem %d", freeRam());
+               logDebug(buffer2);
+            }
+          } else {
+              // cmd/etagexx/debug
+              sprintf(topic, "cmd/%s/debug", arduinoId);
+              if(strcmp(receivedTopic, topic) == 0){
+                sprintf(buffer, "true");
+                if(strcmp(receivedPayload, buffer) == 0){
+                  debug=true;
+                  sprintf(buffer, "Log mode activated");
+                  logDebug(buffer);
+                } else {
+                  debug=false;
+                  sprintf(buffer, "Log mode desactivated");
+                  logDebug(buffer);
+                }
+              } else {
+                  logError(receivedTopic);
+              }
+          }
       }
       messageReceived = false;
   }
+}
+int freeRam () {
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
